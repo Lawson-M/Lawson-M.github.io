ade7e75982354c36762954cf28504dc8e86e97b1
Lawson-M <ltmeridith@gmail.com>
2024-05-29 09:54:41 -0500

    fixed header

--- js/header.js
+++ js/header.js
@@ -5,7 +5,7 @@ header.innerHTML = `
                     <img src=./images/tinylogo.webp class="tinylogo"> </img>
                 </a>
                 <a class="global-header-link" href="About.html">About</a>
-                <a class="global-header-link" href="Index.html#all-projects">All Projects</a>
+                <a class="global-header-link" href="index.html#all-projects">All Projects</a>
                 <a class="global-header-link" href="https://github.com/Lawson-M">Github</a>
             </div>
         </header>
